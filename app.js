0e068a4beaefcc2cb920b337ed07cd8d91c0ddfd
ryumafuma <ryumafuma@hotmail.com>
2022-08-26 16:09:02 +0200

    fin*

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.APP_PORT ?? 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+const userHandlers = require("./userHandlers.js");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id, userHandlers.getUsersById");
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
