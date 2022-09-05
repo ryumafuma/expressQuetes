91b45dbbe2d8a90a08e16ffffeb29f9437711138
ryumafuma <ryumafuma@hotmail.com>
2022-09-05 16:27:07 +0200

    express04bis

--- app.js
+++ app.js
@@ -18,17 +18,27 @@ const moviesHandlers = require("./moviesHandlers");
 
 app.get("/api/movies", moviesHandlers.getMovies);
 app.get("/api/movies/:id", moviesHandlers.getMoviesById);
-app.post("/api/movies", moviesHandlers.postMovies);
-app.put("/api/movies/:id", moviesHandlers.updateMovies);
+//app.post("/api/movies", moviesHandlers.postMovies);
+//app.put("/api/movies/:id", moviesHandlers.updateMovies);
 app.delete("/api/movies/:id", moviesHandlers.deleteMovies);
 
 const usersHandlers = require("./usersHandlers.js");
 
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
-app.post("/api/users", usersHandlers.postUsers);
-app.put("/api/users/:id", usersHandlers.putUsers);
+//app.post("/api/users", usersHandlers.postUsers);
+//app.put("/api/users/:id", usersHandlers.putUsers);
 app.delete("/api/users/:id", usersHandlers.deleteUsers);
+
+const { validateMovies } = require("./validators.js");
+
+app.post("/api/movies", validateMovies, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovies, moviesHandlers.updateMovies);
+
+const { validateUsers } = require("./validators.js");
+
+app.post("/api/users", validateUsers, usersHandlers.postUsers);
+app.put("/api/users/:id", validateUsers, usersHandlers.putUsers);
 
 app.listen(port, (err) => {
   if (err) {
